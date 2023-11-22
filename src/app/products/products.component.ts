9db58ac328e4fdfc54aa48d524dc56f38d01c36b
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2023-11-22 18:02:16 +0530

    feat: filter working !!

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  FormsModule,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 
 interface Product {
+  image: string;
   title: string;
   releaseDate: Date;
   code: string;
@@ -19,16 +25,24 @@ interface Product {
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule,MatFormFieldModule,ScrollingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    ScrollingModule,
+  ],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
-  readonly baseUrl = 'http://localhost:4200/assets/';
+  baseUrl: string = 'http://localhost:4200/assets/';
+  filteredProducts: Product[] = [];
+  searchTerm = '';
+  currentFilter = 'hot'; // Keep track of the current filter
 
-  products=[
+  products: Product[] = [
     {
-      image:`${this.baseUrl}p1.jpg`,
+      image: `${this.baseUrl}p1.jpg`,
       title: 'Product 1',
       releaseDate: new Date('2023-01-01'),
       code: 'P1001',
@@ -36,11 +50,11 @@ export class ProductsComponent {
       sales: 150,
       stock: {
         itemsInStock: 50,
-        variantsInStock: 2
-      }
+        variantsInStock: 2,
+      },
     },
     {
-      image:`${this.baseUrl}p2.jpg`,
+      image: `${this.baseUrl}p2.jpg`,
       title: 'Product 2',
       releaseDate: new Date('2023-02-15'),
       code: 'P1002',
@@ -48,11 +62,11 @@ export class ProductsComponent {
       sales: 200,
       stock: {
         itemsInStock: 75,
-        variantsInStock: 3
-      }
+        variantsInStock: 3,
+      },
     },
     {
-      image:`${this.baseUrl}p1.jpg`,
+      image: `${this.baseUrl}p1.jpg`,
       title: 'Product 1',
       releaseDate: new Date('2023-01-01'),
       code: 'P1001',
@@ -60,11 +74,11 @@ export class ProductsComponent {
       sales: 150,
       stock: {
         itemsInStock: 50,
-        variantsInStock: 2
-      }
+        variantsInStock: 2,
+      },
     },
     {
-      image:`${this.baseUrl}p2.jpg`,
+      image: `${this.baseUrl}p2.jpg`,
       title: 'Product 2',
       releaseDate: new Date('2023-02-15'),
       code: 'P1002',
@@ -72,20 +86,52 @@ export class ProductsComponent {
       sales: 200,
       stock: {
         itemsInStock: 75,
-        variantsInStock: 3
-      }
-    }
+        variantsInStock: 3,
+      },
+    },
   ];
 
-
-  applyForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl('')
-  });
-
-  setProductType(productType:string){
-    console.log("Product Type:",productType)
+  ngOnInit() {
+    this.currentFilter = 'hot'; // Set the default filter to 'hot'
+    this.applyFilters();
   }
 
+  setFilter(filterType: string) {
+    this.currentFilter = filterType;
+    this.applyFilters();
+  }
+
+  applyFilters() {
+    let tempProducts = [...this.products];
+
+    // Apply filter based on filter type
+    switch (this.currentFilter) {
+      case 'hot':
+        tempProducts = tempProducts.sort((a, b) => b.sales - a.sales);
+        break;
+      case 'upcoming':
+        tempProducts = tempProducts.sort(
+          (a, b) =>
+            new Date(a.releaseDate).getTime() -
+            new Date(b.releaseDate).getTime()
+        );
+        break;
+      // No need for 'default' as tempProducts is already a copy of all products
+    }
+
+    // Apply search term
+    if (this.searchTerm) {
+      tempProducts = tempProducts.filter(
+        (product) =>
+          product.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          product.code.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+
+    this.filteredProducts = [...tempProducts];
+  }
+
+  setProductType(productType: string) {
+    console.log('Product Type:', productType);
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.{html,ts}'],
-  },
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },
