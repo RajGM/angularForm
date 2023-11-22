0fe3ccda8fa5be2e003b65c213812724aa98df2d
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2023-11-22 10:58:34 +0530

    feat: products page displaying meta data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'products', component: ProductsComponent, title:"Product List" },
+    { path: 'create-product', component: CreateProductComponent, title:"Create Product" },
+    { path: '', redirectTo: '/products', pathMatch: 'full' } // Redirect to 'products' as the default route
+  ];
+  
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+interface Product {
+  title: string;
+  releaseDate: Date;
+  code: string;
+  numberOfVariants: number;
+  sales: number;
+  stock: {
+    itemsInStock: number;
+    variantsInStock: number;
+  };
+}
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule,MatFormFieldModule,ScrollingModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+  products=[
+    {
+      title: 'Product 1',
+      releaseDate: new Date('2023-01-01'),
+      code: 'P1001',
+      numberOfVariants: 3,
+      sales: 150,
+      stock: {
+        itemsInStock: 50,
+        variantsInStock: 2
+      }
+    },
+    {
+      title: 'Product 2',
+      releaseDate: new Date('2023-02-15'),
+      code: 'P1002',
+      numberOfVariants: 5,
+      sales: 200,
+      stock: {
+        itemsInStock: 75,
+        variantsInStock: 3
+      }
+    },
+  ];
+
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  setProductType(productType:string){
+    console.log("Product Type:",productType)
+  }
+
+}
